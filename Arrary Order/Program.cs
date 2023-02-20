25269fe1c0d8ee10cd09639b1df78578afab5030
g.macharashvili <g.macharashvili@gt.ge>
2023-02-20 11:34:08 +0400

    Add project files.

---
+++ Arrary Order/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Arrary_Order
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 6, 5, 4, 3, 2, 1 };
+            SortArray.Sort(array);
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+        }        
+    }
+}
---
+++ Arrary Order/SortArray.cs
@@ -0,0 +1,28 @@
+﻿namespace Arrary_Order
+{
+    public static class SortArray
+    {
+        public static void Sort(int[] array)
+        {           
+            int reduceSize = 1;
+            for (int j = 0; j < array.Length; j++)
+            {
+                for (int i = 0; i < array.Length - reduceSize; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(array,i);
+                    }
+                }
+                reduceSize++;
+            }
+        }
+
+        private static void Swap(int[] array, int i)
+        {           
+            int value= array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = value;            
+        }
+    }
+}
